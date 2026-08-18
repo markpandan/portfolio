589db0029713332f5fcef788bd57cd984ebe8cd0
markpandan <72681401+markpandan@users.noreply.github.com>
2026-08-18 15:35:13 +0000

    Add allowScripts to enhance npm installation

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,32 +4,32 @@ import { loadSlim } from "@tsparticles/slim";
 import particleOptions from "../utils/particleOptions";
 
 const Background = ({ className }) => {
-  const [init, setInit] = useState(false);
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
+   const [init, setInit] = useState(false);
+   useEffect(() => {
+      initParticlesEngine(async (engine) => {
+         await loadSlim(engine);
+      }).then(() => {
+         setInit(true);
+      });
+   }, []);
 
-  const particlesLoaded = (container) => {
-    console.log("Particles Loaded:", container);
-  };
+   const particlesLoaded = (container) => {
+      console.log("Particles Loaded:", container);
+   };
 
-  const options = useMemo(() => particleOptions, []);
+   const options = useMemo(() => particleOptions, []);
 
-  return (
-    <div className={className}>
-      {init && (
-        <Particles
-          id="tsparticles"
-          options={options}
-          particlesLoaded={particlesLoaded}
-        />
-      )}
-    </div>
-  );
+   return (
+      <div className={className}>
+         {init && (
+            <Particles
+               id="tsparticles"
+               options={options}
+               particlesLoaded={particlesLoaded}
+            />
+         )}
+      </div>
+   );
 };
 
 export default Background;
